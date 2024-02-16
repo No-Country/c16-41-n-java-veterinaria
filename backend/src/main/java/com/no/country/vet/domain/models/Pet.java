82d1131f218776cc8de202aadcac9addf105f500
ahincho <ahincho@unsa.edu.pe>
2024-02-16 16:47:51 -0500

    Project Started

---
+++ backend/src/main/java/com/no/country/vet/domain/models/Pet.java
@@ -0,0 +1,12 @@
+package com.no.country.vet.domain.models;
+
+import java.time.LocalDate;
+
+public class Pet {
+    private Long id;
+    private String name;
+    private String specie;
+    private String breed;
+    private LocalDate birthday;
+    private Long owner;
+}
