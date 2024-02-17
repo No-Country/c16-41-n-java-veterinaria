b8eff9cfd8eb7a4a6a5e44937d6819f9e6f2bd1c
Jerson Reyes <jreyes@bancolombia.com.co>
2024-02-17 11:38:26 -0500

    Se crean los models Pet, User, Service y Appointment

--- backend/src/main/java/com/no/country/vet/domain/models/Pet.java
+++ backend/src/main/java/com/no/country/vet/domain/models/Pet.java
@@ -3,10 +3,10 @@ package com.no.country.vet.domain.models;
 import java.time.LocalDate;
 
 public class Pet {
-    private Long id;
+    private String id;
     private String name;
     private String specie;
     private String breed;
     private LocalDate birthday;
-    private Long owner;
+    private String ownerId;
 }
---
+++ backend/src/main/java/com/no/country/vet/domain/models/User.java
@@ -0,0 +1,11 @@
+package com.no.country.vet.domain.models;
+
+public class User {
+    private String id;
+    private String name;
+    private String email;
+    private String password;
+    private String role;
+    private String address;
+    private String phone;
+}
